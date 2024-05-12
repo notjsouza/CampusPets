8f18e273658236fcdd514d0e0a116d6edc613d6a
Justin <44349024+notjsouza@users.noreply.github.com>
2024-05-12 04:39:19 -0700

    Initial Commit

---
+++ CampusPets/CampusPetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusPetsApp.swift
+//  CampusPets
+//
+//  Created by Justin Souza on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CampusPetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
